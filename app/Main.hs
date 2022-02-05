9232437b22389c0d8ec78834501494b7a21e6c3f
Hiroki Tokunaga <tokusan441@gmail.com>
2022-02-05 21:54:27 +0900

    chore: print a number with animations

--- app/Main.hs
+++ app/Main.hs
@@ -1,78 +1,92 @@
-{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main
-  ( main
-  ) where
+  ( main,
+  )
+where
 
-import qualified Codec.Picture                   as Pic
-import           Codec.Picture.Extra             (crop)
-import           Control.Lens                    (makeLenses, (&), (+~), (^.))
-import qualified Data.ByteString                 as B
-import qualified Data.Text                       as T
-import           Data.Vector.Storable.ByteString (vectorToByteString)
-import           Linear.V2                       (V2 (V2))
-import           Monomer                         (AppEventResponse,
-                                                  EventResponse (Model),
-                                                  Size (Size), WidgetEnv,
-                                                  WidgetNode, appFontDef,
-                                                  appTheme, darkTheme, imageMem,
-                                                  keystroke, label, startApp,
-                                                  vstack)
-import           TextShow                        (TextShow (showt))
+import qualified Codec.Picture as Pic
+import Codec.Picture.Extra (crop)
+import Control.Lens (makeLenses, (%~), (&), (+~), (-~), (.~), (^.))
+import qualified Data.ByteString as B
+import Data.Text (pack)
+import qualified Data.Text as T
+import Data.Vector.Storable.ByteString (vectorToByteString)
+import Linear.V2 (V2 (V2))
+import Monomer
+  ( AnimationMsg (AnimationFinished, AnimationStart, AnimationStop),
+    AppEventResponse,
+    CmbHeight (height),
+    CmbOnFinished (onFinished),
+    CmbStyleBasic (styleBasic),
+    CmbTextSize (textSize),
+    EventResponse (Event, Message, Model),
+    Size (Size),
+    WidgetEnv,
+    WidgetKey (WidgetKey),
+    WidgetNode,
+    animFadeIn,
+    animFadeOut,
+    animFadeOut_,
+    appFontDef,
+    appTheme,
+    darkTheme,
+    hstack,
+    imageMem,
+    keystroke,
+    label,
+    nodeKey,
+    nodeVisible,
+    startApp,
+    vstack,
+  )
+import TextShow (TextShow (showt))
 
-data AppModel =
-  AppModel
-    { _position :: V2 Int
-    , _name     :: Int
-    , _dogImage :: Pic.Image Pic.PixelRGBA8
-    }
+data AppModel = AppModel
+  {_index :: Int, _changing :: Bool, _visible :: Bool}
   deriving (Eq)
 
 makeLenses ''AppModel
 
 data Event
-  = MoveUp
-  | MoveDown
-  | MoveRight
-  | MoveLeft
+  = ShowNext
+  | Update
+  | Update2
 
 handleEvent ::
-     WidgetEnv AppModel Event
-  -> WidgetNode AppModel Event
-  -> AppModel
-  -> Event
-  -> [AppEventResponse AppModel Event]
+  WidgetEnv AppModel Event ->
+  WidgetNode AppModel Event ->
+  AppModel ->
+  Event ->
+  [AppEventResponse AppModel Event]
 handleEvent _ _ model event =
   case event of
-    MoveUp    -> [Model $ model & position +~ V2 0 1 & name +~ 1]
-    MoveDown  -> [Model $ model & position +~ V2 0 (-1) & name +~ 1]
-    MoveRight -> [Model $ model & position +~ V2 1 0 & name +~ 1]
-    MoveLeft  -> [Model $ model & position +~ V2 (-1) 0 & name +~ 1]
+    ShowNext
+      | not $ model ^. changing ->
+          [ Model $ model & changing .~ True,
+            Message (WidgetKey "animFadeIn0") AnimationStop,
+            Message (WidgetKey "animFadeOut0") AnimationStart
+          ]
+      | otherwise -> []
+    Update ->
+      [ Event Update2,
+        -- Model $ model & visible .~ False,
+        Message (WidgetKey "animFadeIn0") AnimationStart
+      ]
+    Update2 -> [Model $ model & index +~ 1 & changing .~ False & visible .~ True]
 
 buildUI :: WidgetEnv AppModel Event -> AppModel -> WidgetNode AppModel Event
 buildUI wenv model = widgetTree
   where
     widgetTree =
-      withKeys $
-      vstack
-        [ label $ T.pack $ "Position: " <> show (model ^. position)
-        , imageMem (showt $ model ^. name) byteImage $
-          Size (fromIntegral lenX) (fromIntegral lenY)
-        ]
-    withKeys =
-      keystroke
-        [ ("Up", MoveUp)
-        , ("Down", MoveDown)
-        , ("Right", MoveRight)
-        , ("Left", MoveLeft)
-        ]
-    byteImage =
-      vectorToByteString $
-      Pic.imageData $ crop posX posY lenX lenY $ model ^. dogImage
-    V2 posX posY = model ^. position
-    V2 lenX lenY = clipSize
+      withKeys $ dualAnim 0 $ label (showt $ model ^. index) `styleBasic` [textSize 128, height 128] `nodeVisible` (model ^. visible)
+    withKeys = keystroke [("Enter", ShowNext)]
+    dualAnim n c = outer
+      where
+        inner = animFadeIn c `nodeKey` pack ("animFadeIn" ++ show n)
+        outer = animFadeOut_ [onFinished Update] inner `nodeKey` pack ("animFadeOut" ++ show n)
 
 clipSize :: V2 Int
 clipSize = V2 100 100
@@ -80,12 +94,13 @@ clipSize = V2 100 100
 main :: IO ()
 main = do
   img <-
-    Pic.convertRGBA8 .
-    (\case
-       Right x -> x
-       Left _  -> error "Failed to load the image.") <$>
-    Pic.readImage "./dog.jpg"
-  let model = AppModel (V2 100 100) 0 img
+    Pic.convertRGBA8
+      . ( \case
+            Right x -> x
+            Left _ -> error "Failed to load the image."
+        )
+      <$> Pic.readImage "./dog.jpg"
+  let model = AppModel 0 False True
   startApp
     model
     handleEvent
